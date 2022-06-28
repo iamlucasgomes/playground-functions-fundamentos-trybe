abf6524d5724944331729a8f5ac40d03162c2fc3
iamlucasgomes <lucas.devjs@gmail.com>
2022-06-27 23:14:52 -0300

    UPDATE: encode/decode funções encode & decode completa

--- src/challenges.js
+++ src/challenges.js
@@ -71,11 +71,20 @@ function fizzBuzz(arrayOfNumbers) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(string) {
+  return string.replace(/a/gi, 1)
+    .replace(/e/gi, 2)
+    .replace(/i/gi, 3)
+    .replace(/o/gi, 4)
+    .replace(/u/gi, 5);
 }
-function decode() {
-  // seu código aqui
+
+function decode(string) {
+  return string.replace(/1/g, 'a')
+    .replace(/2/g, 'e')
+    .replace(/3/g, 'i')
+    .replace(/4/g, 'o')
+    .replace(/5/g, 'u');
 }
 
 // Desafio 10
